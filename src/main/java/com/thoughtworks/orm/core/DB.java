5d7adb305f874f9c8548a0867f1c1e99281aae1b
Cui Liqiang <cui.liqiang@gmail.com>
2013-05-14 21:16:30 +0800

    finish find by id

--- src/main/java/com/thoughtworks/orm/core/DB.java
+++ src/main/java/com/thoughtworks/orm/core/DB.java
@@ -1,19 +1,27 @@
 package com.thoughtworks.orm.core;
 
+import com.sun.xml.internal.ws.util.StringUtils;
 import com.thoughtworks.orm.annotation.PrimaryKey;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Types;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DB {
     private static Map<String, ConnectionInfo> connectionInfoMap = new HashMap<String, ConnectionInfo>();
     private static Map<String, DB> connectionMap = new HashMap<String, DB>();
+    private static final String GETTER_PATTERN = "get[A-Z].*";
+    private static final String SETTER_PATTERN = "set[A-Z].*";
 
     static {
         try {
@@ -31,18 +39,18 @@ public class DB {
     }
 
     public static DB connect(String dbName) throws ClassNotFoundException, SQLException {
-        DB db = connectionMap.get("dbName");
+        DB db = connectionMap.get(dbName);
         if (db == null) {
             Connection connect = connectionInfoMap.get(dbName).connect();
-            connectionMap.put("dbName", new DB(connect));
+            connectionMap.put(dbName, new DB(connect));
         }
-        return connectionMap.get("dbName");
+        return connectionMap.get(dbName);
     }
 
     public void save(Object obj) throws Exception {
         StringBuffer prepareString = new StringBuffer("insert into " + obj.getClass().getSimpleName() +"s (");
 
-        Method[] methods = obj.getClass().getDeclaredMethods();
+        List<Method> methods = filterByPattern(obj.getClass().getDeclaredMethods(), GETTER_PATTERN);
 
         addColumnNamesTo(prepareString, methods);
         prepareString.append(") values (");
@@ -53,18 +61,49 @@ public class DB {
         addValuesForPlaceHolders(obj, methods, preparedStatement);
 
         preparedStatement.executeUpdate();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT @@IDENTITY AS 'id'");
+        if(resultSet.next()){
+            obj.getClass().getMethod("setId", int.class).invoke(obj, resultSet.getInt("id"));
+        }
     }
 
-    @Override
-    protected void finalize() throws Throwable {
-        connection.close();
-        super.finalize();
+    private ArrayList<Method> filterByPattern(Method[] declaredMethods, String pattern) {
+        ArrayList<Method> methods = new ArrayList<Method>();
+        for (Method declaredMethod : declaredMethods) {
+            if(declaredMethod.getName().matches(pattern)) {
+                methods.add(declaredMethod);
+            }
+        }
+        return methods;
     }
 
-    private void addValuesForPlaceHolders(Object obj, Method[] methods, PreparedStatement preparedStatement) throws Exception {
+    public <T> T find(Class<T> clazz, int id) throws SQLException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+        Statement statement = connection.createStatement();
+        statement.executeQuery("select * from " + clazz.getSimpleName() +"s where id =" + id + ";");
+        ResultSet resultSet = statement.getResultSet();
+
+        List<Method> methods = filterByPattern(clazz.getDeclaredMethods(), SETTER_PATTERN);
+        T t = clazz.newInstance();
+
+        if(resultSet.next()) {
+            for (Method method : methods) {
+                if (shouldNotBeInSql(method)) continue;
+
+                String simpleName = method.getParameterTypes()[0].getSimpleName();
+
+                Object value = resultSet.getClass().getMethod("get" + StringUtils.capitalize(simpleName), String.class)
+                        .invoke(resultSet, getPropertyName(method));
+                method.invoke(t, value);
+            }
+        }
+        return t;
+    }
+
+    private void addValuesForPlaceHolders(Object obj, List<Method> methods, PreparedStatement preparedStatement) throws Exception {
         int index = 1;
         for (Method method : methods) {
-            if (shouldBeInSql(method)) continue;
+            if (shouldNotBeInSql(method)) continue;
 
             Object value = method.invoke(obj);
             if(value == null) {
@@ -75,22 +114,32 @@ public class DB {
         }
     }
 
-    private void addPlaceHolders(StringBuffer prepareString, Method[] methods) {
+    private void addPlaceHolders(StringBuffer prepareString, List<Method> methods) {
         for (Method method : methods) {
-            if (shouldBeInSql(method)) continue;
+            if (shouldNotBeInSql(method)) continue;
 
             prepareString.append("?,");
         }
         prepareString.replace(prepareString.length() - 1, prepareString.length(), ")");
     }
 
-    private void addColumnNamesTo(StringBuffer prepareString, Method[] methods) {
+    private void addColumnNamesTo(StringBuffer prepareString, List<Method> methods) {
         for (Method method : methods) {
-            if (shouldBeInSql(method)) continue;
+            if (shouldNotBeInSql(method)) continue;
 
-            prepareString.append(method.getName().substring(3) + ",");
+            prepareString.append(getPropertyName(method) + ",");
         }
         prepareString.replace(prepareString.length() - 1, prepareString.length(), "");
+    }
+
+    private String getPropertyName(Method method) {
+        return method.getName().substring(3);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        connection.close();
+        super.finalize();
     }
 
     private int sqlTypeId(Class<?> type) throws Exception {
@@ -102,10 +151,9 @@ public class DB {
         throw new Exception("Do not know what sql type to use for " + type.getName());
     }
 
-    private boolean shouldBeInSql(Method method) {
+    private boolean shouldNotBeInSql(Method method) {
         return method.getAnnotation(PrimaryKey.class) != null
-                ||!Modifier.isPublic(method.getModifiers())
-                ||!method.getName().matches("get[A-Z].*");
+                ||!Modifier.isPublic(method.getModifiers());
     }
 
     private void setValue(PreparedStatement preparedStatement, Object value, int index) throws SQLException {
--- src/test/java/com/thoughtworks/orm/core/DBTest.java
+++ src/test/java/com/thoughtworks/orm/core/DBTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.orm.core;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import test.domains.Comment;
 import util.DBConnectionTestUtil;
@@ -8,21 +9,29 @@ import util.DBConnectionTestUtil;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class DBTest {
     DBConnectionTestUtil util = new DBConnectionTestUtil();
+    DB product;
+
+    @Before
+    public void setUp() throws ClassNotFoundException, SQLException {
+        product = DB.connect("product");
+    }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws ClassNotFoundException, SQLException {
         util.executeUpdate("delete from comments;");
         util.close();
     }
 
     @Test
     public void should_save_object_with_all_fields_set() throws Exception {
-        DB product = DB.connect("product");
         Comment comment = new Comment();
         comment.setMyUser("Liqiang");
         comment.setSummery("good");
@@ -35,8 +44,20 @@ public class DBTest {
     }
 
     @Test
+    public void should_fill_id_after_saved() throws Exception {
+        Comment comment = new Comment();
+        comment.setMyUser("Liqiang");
+        comment.setSummery("good");
+        comment.setWebPage("home page");
+        comment.setEmail("xx@xx.com");
+        comment.setComments("comment");
+
+        product.save(comment);
+        assertTrue(comment.getId() != 0);
+    }
+
+    @Test
     public void should_save_object_with_not_all_fields_set() throws Exception {
-        DB product = DB.connect("product");
         Comment comment = new Comment();
         comment.setMyUser("Liqiang");
         comment.setSummery("good");
@@ -45,6 +66,28 @@ public class DBTest {
 
         product.save(comment);
         verifyObject(comment);
+    }
+
+    @Test
+    public void should_be_able_to_get_object_from_db_by_id() throws Exception {
+        Comment comment = new Comment();
+        comment.setMyUser("Liqiang");
+        comment.setSummery("good");
+        comment.setWebPage("home page");
+        comment.setComments("comment");
+        product.save(comment);
+
+        Comment commentInDb = product.find(Comment.class, comment.getId());
+        assertObjsEquality(comment, commentInDb);
+    }
+
+    private void assertObjsEquality(Comment comment, Comment commentInDb) {
+        assertEquals(comment.getId(), commentInDb.getId());
+        assertEquals(comment.getMyUser(), commentInDb.getMyUser());
+        assertEquals(comment.getEmail(), commentInDb.getEmail());
+        assertEquals(comment.getWebPage(), commentInDb.getWebPage());
+        assertEquals(comment.getSummery(), commentInDb.getSummery());
+        assertEquals(comment.getComments(), commentInDb.getComments());
     }
 
     private void verifyObject(Comment comment) throws SQLException {
